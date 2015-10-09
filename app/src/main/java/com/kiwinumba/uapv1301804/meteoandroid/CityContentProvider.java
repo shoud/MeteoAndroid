f535db59c4d38c99c953201e3a48c1b7021ff0ea
shoud <shoud@hotmail.fr>
2015-10-09 15:12:11 +0200

    push critique probleme sur pc pas fini provider

---
+++ app/src/main/java/com/kiwinumba/uapv1301804/meteoandroid/CityContentProvider.java
@@ -0,0 +1,93 @@
+package com.kiwinumba.uapv1301804.meteoandroid;
+
+import android.content.ContentProvider;
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.net.Uri;
+
+import java.util.HashMap;
+
+/**
+ * Created by uapv1301804 on 09/10/15.
+ */
+public class CityContentProvider extends ContentProvider
+{
+
+    //Le nom du provider
+    static final String PROVIDER_NAME = "com.kiwinumba.uapv1301804.provider.meteoandroid";
+    public static final Uri CONTENT_URI = new Uri.Builder().scheme(ContentResolver.SCHEME_CONTENT)
+            .authority(PROVIDER_NAME)
+            .appendEncodedPath(WeatherDatabase.TABLE_WEATHER)
+            .build();
+
+    // fields for the database
+    static final String CITY_KEY = "id";
+    static final String CITY_NOM = "nom";
+    static final String CITY_PAYS = "pays";
+    static final String CITY_VENT = "vent";
+    static final String CITY_TEMP = "temperature";
+    static final String CITY_PRES = "pression";
+    static final String CITY_DATE = "date";
+
+    // integer values used in content URI
+    static final int FRIENDS = 1;
+    static final int FRIENDS_ID = 2;
+
+    // projection map for a query
+    private static HashMap<String, String> BirthMap;
+
+    // maps content URI "patterns" to the integer values that were set above
+    static final UriMatcher uriMatcher;
+    static{
+        uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+        uriMatcher.addURI(PROVIDER_NAME, "friends", FRIENDS);
+        uriMatcher.addURI(PROVIDER_NAME, "friends/#", FRIENDS_ID);
+    }
+
+    // database declarations
+    private SQLiteDatabase database;
+    static final String DATABASE_NAME = "BirthdayReminder";
+    static final String TABLE_NAME = "birthTable";
+    static final int DATABASE_VERSION = 1;
+    static final String CREATE_TABLE =
+            " CREATE TABLE " + TABLE_NAME +
+                    " (id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    " name TEXT NOT NULL, " +
+                    " birthday TEXT NOT NULL);";
+
+
+    @Override
+    public boolean onCreate() {
+        return false;
+    }
+
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+        return null;
+    }
+
+    @Override
+    public String getType(Uri uri) {
+        return null;
+    }
+
+    @Override
+    public Uri insert(Uri uri, ContentValues values) {
+        return null;
+    }
+
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        return 0;
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+        return 0;
+    }
+}

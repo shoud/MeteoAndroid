9e3cb0770da3a9550d606df9f3fe131b4b9dac90
shoud <shoud@hotmail.fr>
2015-10-09 15:47:04 +0200

    Provider pas fini

--- app/src/main/java/com/kiwinumba/uapv1301804/meteoandroid/CityContentProvider.java
+++ app/src/main/java/com/kiwinumba/uapv1301804/meteoandroid/CityContentProvider.java
@@ -18,6 +18,8 @@ import java.util.HashMap;
 public class CityContentProvider extends ContentProvider
 {
 
+    //Lien util http://www.tutos-android.com/contentprovider-android
+    //private DatabaseHelper dbHelper;
     //Le nom du provider
     static final String PROVIDER_NAME = "com.kiwinumba.uapv1301804.provider.meteoandroid";
     public static final Uri CONTENT_URI = new Uri.Builder().scheme(ContentResolver.SCHEME_CONTENT)
@@ -25,10 +27,35 @@ public class CityContentProvider extends ContentProvider
             .appendEncodedPath(Bdd.CITY_TABLE_NAME)
             .build();
 
+    private static final int WEATHER = 1;
+    private static final int WEATHER_PAYS_VILLE = 2;
+
+    private static final UriMatcher sURIMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+    static
+    {
+        sURIMatcher.addURI(PROVIDER_NAME, "weather", WEATHER);
+        sURIMatcher.addURI(PROVIDER_NAME, "weather/*/*", WEATHER_PAYS_VILLE);
+    }
+
+    public String getCity(Uri url)
+    {
+        int match = sURIMatcher.match(url);
+        switch (match)
+        {
+            case WEATHER:
+                return "vnd.android.cursor.dir/weather";
+            case WEATHER_PAYS_VILLE:
+                return "vnd.android.cursor.item/weather";
+            default:
+                return null;
+        }
+    }
+
 
     @Override
     public boolean onCreate() {
-        return false;
+        return false ;
     }
 
     @Override

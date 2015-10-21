7090bf6c7fd385f67f3e070dadc0944226a0b11d
shoud <shoud@hotmail.fr>
2015-10-21 15:16:34 +0200

    Refaire en fonction de ce lien : http://www.vogella.com/tutorials/AndroidSQLite/article.html#contentprovider

--- app/src/main/java/com/kiwinumba/uapv1301804/meteoandroid/CityContentProvider.java
+++ app/src/main/java/com/kiwinumba/uapv1301804/meteoandroid/CityContentProvider.java
@@ -3,40 +3,43 @@ package com.kiwinumba.uapv1301804.meteoandroid;
 import android.content.ContentProvider;
 import android.content.ContentResolver;
 import android.content.ContentValues;
-import android.content.Context;
 import android.content.UriMatcher;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteQueryBuilder;
 import android.net.Uri;
 
-import java.util.HashMap;
 
-/**
- * Created by uapv1301804 on 09/10/15.
- */
 public class CityContentProvider extends ContentProvider
 {
 
     //Lien util http://www.tutos-android.com/contentprovider-android
     //private DatabaseHelper dbHelper;
-    //Le nom du provider
-    static final String PROVIDER_NAME = "com.kiwinumba.uapv1301804.provider.meteoandroid";
-    public static final Uri CONTENT_URI = new Uri.Builder().scheme(ContentResolver.SCHEME_CONTENT)
-            .authority(PROVIDER_NAME)
-            .appendEncodedPath(Bdd.CITY_TABLE_NAME)
-            .build();
 
-    private static final int WEATHER = 1;
-    private static final int WEATHER_PAYS_VILLE = 2;
+    //La base de donnnées
+    private Bdd bdd;
+    //Utilisé pour le UriMacher
+    private static final int WEATHER = 10;
+    private static final int WEATHER_PAYS_VILLE = 20;
+    //L'autorité du provider
+    private static final String PROVIDER_AUTHORITY = "com.kiwinumba.uapv1301804.provider.meteoandroid";
+    private static final String BASE_PATH = "weather";
+
+    public static final Uri CONTENT_URI = Uri.parse("content://" + PROVIDER_AUTHORITY
+            + "/" + BASE_PATH);
+
+
+    public static final String CONTENT_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE
+            + "/weather";
+    public static final String CONTENT_ITEM_TYPE = ContentResolver.CURSOR_ITEM_BASE_TYPE
+            + "/City";
 
     private static final UriMatcher sURIMatcher = new UriMatcher(UriMatcher.NO_MATCH);
-
-    static
-    {
-        sURIMatcher.addURI(PROVIDER_NAME, "weather", WEATHER);
-        sURIMatcher.addURI(PROVIDER_NAME, "weather/*/*", WEATHER_PAYS_VILLE);
+    static {
+        sURIMatcher.addURI(PROVIDER_AUTHORITY, BASE_PATH, WEATHER);
+        sURIMatcher.addURI(PROVIDER_AUTHORITY, BASE_PATH + "/*/*", WEATHER_PAYS_VILLE);
     }
+
 
     public String getCity(Uri url)
     {
@@ -54,13 +57,40 @@ public class CityContentProvider extends ContentProvider
 
 
     @Override
-    public boolean onCreate() {
+    public boolean onCreate()
+    {
+        bdd = new Bdd(getContext());
         return false ;
     }
 
     @Override
     public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
-        return null;
+
+        SQLiteQueryBuilder queryBuilder = new SQLiteQueryBuilder();
+        checkColumns(projection);
+
+        queryBuilder.setTables(Bdd.CITY_TABLE_NAME);
+
+        int uriType = sURIMatcher.match(uri);
+        switch (uriType) {
+            case WEATHER:
+                break;
+            case WEATHER_PAYS_VILLE:
+                // adding the ID to the original query
+                queryBuilder.appendWhere(Bdd.COLUMN_ID + "="
+                        + uri.getLastPathSegment());
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown URI: " + uri);
+        }
+
+        SQLiteDatabase db = bdd.getWritableDatabase();
+        Cursor cursor = queryBuilder.query(db, projection, selection,
+                selectionArgs, null, null, sortOrder);
+        // make sure that potential listeners are getting notified
+        cursor.setNotificationUri(getContext().getContentResolver(), uri);
+
+        return cursor;
     }
 
     @Override
